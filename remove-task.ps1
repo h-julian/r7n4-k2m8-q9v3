9a3281450a917306b05f05d539ab0a75778c3113
h-julian <151588228+h-julian@users.noreply.github.com>
2026-08-18 00:08:48 +0200

    Monitor ALSA return from Zamora

--- remove-task.ps1
+++ remove-task.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = "Stop"
-$taskName = "ALSA Jerez-Zamora monitor"
+$taskName = "ALSA Zamora-Jerez monitor"
 
 if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
     Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
